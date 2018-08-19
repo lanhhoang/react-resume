451d2927a53d5b5e7682ab2bb5e909a2d17008e9
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-19 21:21:45 +0700

    Add App CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css';
+
+import Profile from './components/Profile/Profile';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className={styles.App}>
+        <div className={styles.SideBar}>
+          <Profile />
+        </div>
+        <div className={styles.ContentWrapper}>Resume</div>
+      </div>
+    );
   }
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const Profile = ({ name, profession, bio }) => {
+const Profile = ({ name, profession }) => {
   return (
     <div>
       <div>Avatar</div>
       <div className={styles.title}>
         <h1>{name}</h1>
         <h2>{profession}</h2>
-      </div>
-      <div className={styles.desc}>
-        <p>{bio}</p>
       </div>
       <div>Social</div>
     </div>
