b029916bc02dccfdf6ea391090d6207bb92cc14f
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-20 23:31:10 +0700

    Import Card component to Education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import styles from './Education.css';
 
+import Card from '../Card/Card';
+
 const Education = ({ educations }) => {
   const myEducation = (
     <div>
       {educations.map(education => {
         return (
-          <div className={styles.Item} key={education.degree}>
-            <h3>{education.university}</h3>
-            <span>
-              {education.startTime} - {education.endTime}
-            </span>
-            <p>
-              {education.degree} {education.fieldOfStudy}
-            </p>
-            <p>{education.description}</p>
-          </div>
+          <Card
+            key={education.degree}
+            header={education.university}
+            metadata={`${education.startTime} - ${education.endTime}`}
+            title={education.degree}
+            subtitle={education.fieldOfStudy}
+            description={education.description}
+          />
         );
       })}
     </div>
