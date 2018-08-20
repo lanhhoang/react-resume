f6112eb61f34d4de676a4221335da81c9e4e2944
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-20 23:37:41 +0700

    Import Experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Experience.css';
+
+import Card from '../Card/Card';
+
+const Experience = ({ experiences }) => {
+  const myExperience = (
+    <div>
+      {experiences.map(experience => {
+        return (
+          <Card
+            key={experience.company}
+            header={education.company}
+            metadata={`${experience.startTime} - ${experience.endTime}`}
+            title={experience.jobTitle}
+            description={experience.description}
+          />
+        );
+      })}
+    </div>
+  );
+  return (
+    <div className={styles.Experience}>
+      <i className="fas fa-briefcase" />
+      <h2>EXPERIENCE</h2>
+      {myExperience}
+    </div>
+  );
+};
+
+export default Experience;
